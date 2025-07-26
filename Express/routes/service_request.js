81db2f3ab44d5154e62992cdbeaf68200edf67ac
Adi715-g <adityagokhale2002@gmail.com>
2025-07-26 19:46:00 +0530

    routes

---
+++ Express/routes/service_request.js
@@ -0,0 +1,15 @@
+
+const db = require("../utils/dbpool")
+const {apiSuccess, apiError} = require("../utils/apiresult")
+const express = require("express")
+const app = express.Router()
+
+
+
+
+
+
+
+
+
+module.exports = app
